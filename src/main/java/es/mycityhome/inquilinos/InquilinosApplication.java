464fe7678f7581c5c905b81d80f0434a6e76e4aa
Alisha <alejandro.ibarra@bootcamp-upgrade.com>
2025-03-21 18:41:26 +0100

    Update pom.xml and InquilinosApplication.java for project configuration and documentation

--- src/main/java/es/mycityhome/inquilinos/InquilinosApplication.java
+++ src/main/java/es/mycityhome/inquilinos/InquilinosApplication.java
@@ -3,6 +3,12 @@ package es.mycityhome.inquilinos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 🚀 Clase principal de la aplicación de gestión de cerraduras
+ * 
+ * Esta clase es el punto de entrada de la aplicación Spring Boot que
+ * proporciona servicios REST para la gestión de cerraduras We-Lock.
+ */
 @SpringBootApplication
 public class InquilinosApplication {
 
